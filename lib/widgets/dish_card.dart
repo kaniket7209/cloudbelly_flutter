c490934dadae6bc489b5d82f1597990be6bb0303
Aniket <kaniket7209@gmail.com>
2024-06-22 20:14:43 +0530

    update

--- lib/widgets/dish_card.dart
+++ lib/widgets/dish_card.dart
@@ -1,5 +1,6 @@
 // lib/widgets/dish_card.dart
 import 'package:cloudbelly_app/screens/Tabs/Profile/profile_view.dart';
+import 'package:figma_squircle/figma_squircle.dart';
 import 'package:flutter/material.dart';
 import '../models/dish.dart';
 
@@ -12,17 +13,6 @@ class DishCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
-      // decoration: BoxDecoration(
-      //   color: Colors.white,
-      //   borderRadius: BorderRadius.circular(10.0),
-      //   boxShadow: [
-      //     BoxShadow(
-      //       color: Colors.black12,
-      //       blurRadius: 6.0,
-      //       offset: Offset(0, 2),
-      //     ),
-      //   ],
-      // ),
       child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Row(
@@ -42,56 +32,115 @@ class DishCard extends StatelessWidget {
                   // You can clear the userId or perform any other actions here if needed
                 });
               },
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child: Image.network(
-                  dish.images.isNotEmpty
-                      ? dish.images.first
-                      : 'https://via.placeholder.com/150',
-                  height: 80,
-                  width: 80,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    return Image.asset(
-                      'assets/placeholder.png',
-                      height: 80,
-                      width: 80,
-                      fit: BoxFit.cover,
-                    );
-                  },
+              child: Container(
+                width: 90,
+                height: 90,
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: SmoothRectangleBorder(
+                    borderRadius: SmoothBorderRadius(
+                      cornerRadius: 17.0,
+                      cornerSmoothing: 1,
+                    ),
+                  ),
+                  shadows: [
+                    BoxShadow(
+                      color: Color(0xff9E749E).withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 20,
+                      offset: Offset(3, 6),
+                    ),
+                  ],
+                  image: DecorationImage(
+                    image: NetworkImage(dish.images.isNotEmpty
+                        ? dish.images.first
+                        : 'https://via.placeholder.com/150'),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-            SizedBox(width: 10),
+            SizedBox(width: 20),
             Expanded(
               child: Column(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Center vertically
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    dish.name,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Color(0xff2E0536),
-                    ),
+                  Row(
+                    children: [
+                      Container(
+                        width : MediaQuery.of(context).size.width * 0.3,
+                        child: Text(
+                          dish.name,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Product Sans',
+                            fontSize: 16,
+                            color: Color(0xff2E0536),
+                          ),
+                        ),
+                      ),
+                      // SizedBox(width: 2),
+                      Container(
+                        width: 16,
+                        height: 16,
+                        decoration: ShapeDecoration(
+                          color: Colors.white,
+                          shape: SmoothRectangleBorder(
+                            borderRadius: SmoothBorderRadius(
+                              cornerRadius: 7.0,
+                              cornerSmoothing: 1,
+                            ),
+                          ),
+                          shadows: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.2),
+                              spreadRadius: 1,
+                              blurRadius: 7,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                          // side: BorderSide(
+                          //   color: Colors.black,
+                          //   width: 2.0,
+                          // ),
+                        ),
+                        child: Center(
+                          child: Container(
+                            width: 9,
+                            height: 9,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: dish.type == 'Veg'
+                                  ? Color(0xff6EFD6B)
+                                  : Color(0xffE23131),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        'Rs ${double.parse(dish.price).toStringAsFixed(0)}',
+                        style: TextStyle(
+                            fontSize: 14.0,
+                            fontFamily: 'Product Sans',
+                            color: Color(0xff9428A9),
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.right,
+                      ),
+                    ],
                   ),
+                 
+                 
+                  
                   SizedBox(height: 5),
                   Text(
-                    'Category: ${dish.category}',
+                    dish.description,
                     style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
-                  Text(
-                    'Type: ${dish.type}',
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
-                  Text(
-                    'Price: ${dish.price}',
-                    style: TextStyle(
-                      color: Colors.grey,
+                      color: Color(0xff9428A9),
+                      fontFamily: 'Product Sans',
+                      fontSize: 12,
                     ),
                   ),
                 ],
--- lib/widgets/restaurant_card.dart
+++ lib/widgets/restaurant_card.dart
@@ -19,112 +19,114 @@ class RestaurantCard extends StatelessWidget {
   }
 
   @override
-Widget build(BuildContext context) {
-  return Container(
-    margin: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
-    child: Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Row(
-        children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ProfileView(
-                    userIdList: [
-                      restaurant.id
-                    ], // Adjust this according to your ProfileView constructor
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileView(
+                      userIdList: [
+                        restaurant.id
+                      ], // Adjust this according to your ProfileView constructor
+                    ),
                   ),
-                ),
-              ).then((value) {
-                // You can clear the userId or perform any other actions here if needed
-              });
-            },
-            child: Container(
-              width: 90,
-              height: 90,
-              decoration: ShapeDecoration(
-                color: Colors.white,
-                shape: SmoothRectangleBorder(
-                  borderRadius: SmoothBorderRadius(
-                    cornerRadius: 17.0,
-                    cornerSmoothing: 1,
+                ).then((value) {
+                  // You can clear the userId or perform any other actions here if needed
+                });
+              },
+              child: Container(
+                width: 90,
+                height: 90,
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: SmoothRectangleBorder(
+                    borderRadius: SmoothBorderRadius(
+                      cornerRadius: 17.0,
+                      cornerSmoothing: 1,
+                    ),
                   ),
-                ),
-                shadows: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
-                    spreadRadius: 1,
-                    blurRadius: 7,
-                    offset: Offset(0, 3),
+                  shadows: [
+                    BoxShadow(
+                     color: Color(0xff9E749E).withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 20,
+                      offset: Offset(3, 6),
+                    ),
+                  ],
+                  image: DecorationImage(
+                    image: NetworkImage(restaurant.profilePhoto.isNotEmpty
+                        ? restaurant.profilePhoto
+                        : 'https://via.placeholder.com/150'),
+                    fit: BoxFit.cover,
                   ),
-                ],
-                image: DecorationImage(
-                  image: NetworkImage(restaurant.profilePhoto.isNotEmpty
-                      ? restaurant.profilePhoto
-                      : 'https://via.placeholder.com/150'),
-                  fit: BoxFit.cover,
                 ),
               ),
             ),
-          ),
-          SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, // Center vertically
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  restaurant.storeName,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                    color: Color(0xff2E0536),
+            SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Center vertically
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    restaurant.storeName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Color(0xff2E0536),
+                    ),
                   ),
-                ),
-                SizedBox(height: 5),
-                Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/Heart.png', // Ensure this asset file exists
-                      width: 20, // Set the width of the image
-                      height: 20, // Set the height of the image
-                    ),
-                    SizedBox(width: 8.0), // Add some space between the image and the text
-                    Text(
-                      '--  --', // Add the rating text here
-                      style: TextStyle(
-                        fontSize: 14.0,
-                        color: Color(0xff9428A9),
-                        fontWeight: FontWeight.bold
+                  SizedBox(height: 5),
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/Heart.png', // Ensure this asset file exists
+                        width: 20, // Set the width of the image
+                        height: 20, // Set the height of the image
                       ),
-                    ),
-                    Spacer(), // Pushes the text to the right
-                    Text(
-                      '45-50 mins',
-                      style: TextStyle(
-                        fontSize: 14.0,
-                        color: Color(0xff9428A9),
-                        fontWeight: FontWeight.bold
+                      SizedBox(
+                          width:
+                              8.0), // Add some space between the image and the text
+                      Text(
+                        '--  --', // Add the rating text here
+                        style: TextStyle(
+                            fontSize: 14.0,
+                            color: Color(0xff9428A9),
+                            fontWeight: FontWeight.bold),
                       ),
-                      textAlign: TextAlign.right,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 5),
-                Text(
-                  restaurant.location,
-                  style: TextStyle(
-                    color: Color(0xff9428A9),
-                    fontSize: 12,
+                      Spacer(), // Pushes the text to the right
+                      Text(
+                        '45-50 mins',
+                        style: TextStyle(
+                            fontSize: 14.0,
+                            color: Color(0xff9428A9),
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.right,
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                  SizedBox(height: 5),
+                  Text(
+                    restaurant.location,
+                    style: TextStyle(
+                      color: Color(0xff9428A9),
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
-}}
+    );
+  }
+}
