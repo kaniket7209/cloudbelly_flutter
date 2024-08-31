9e58dd0b6357c4f8aad571cf40306ecf5d2dc307
Aniket <kaniket7209@gmail.com>
2024-09-01 03:11:37 +0530

    update

--- lib/screens/Tabs/belly_gpt.dart
+++ lib/screens/Tabs/belly_gpt.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:figma_squircle/figma_squircle.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:cloudbelly_app/api_service.dart'; // Assuming you have this file to handle API calls
 
@@ -16,6 +14,9 @@ class _BellyGPTPageState extends State<BellyGPTPage> {
   bool _isLoading = false;
 
   Future<void> _askBellyGPT() async {
+     setState(() {
+      _isLoading = true;
+    });
     final prompt = _promptController.text.trim();
 
     if (prompt.isEmpty) {
@@ -23,9 +24,7 @@ class _BellyGPTPageState extends State<BellyGPTPage> {
       return;
     }
 
-    setState(() {
-      _isLoading = true;
-    });
+   
 
     try {
       final response = await Provider.of<Auth>(context, listen: false)
@@ -59,7 +58,7 @@ class _BellyGPTPageState extends State<BellyGPTPage> {
         line = line.replaceAll("**", "");
         widgets.add(Text(
           line,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, fontFamily: 'Product Sans', color: Color(0xff1B7997)),
         ));
       } else if (RegExp(r'^\d+\..*').hasMatch(line) || line.startsWith('* ')) {
         // Handle list items
@@ -68,7 +67,7 @@ class _BellyGPTPageState extends State<BellyGPTPage> {
           widgets.add(SizedBox(height: 10));
         }
         widgets.add(Text("• ${line.replaceAll(RegExp(r'^\d+\.\s*|\*\s*'), '')}",
-            style: TextStyle(fontSize: 16)));
+            style: TextStyle(fontSize: 16,color: Color(0xff1B7997),fontFamily: 'Product Sans')));
       } else {
         if (isListOpen) {
           isListOpen = false;
@@ -76,7 +75,7 @@ class _BellyGPTPageState extends State<BellyGPTPage> {
         }
         widgets.add(Text(
           line,
-          style: TextStyle(fontSize: 16),
+          style: TextStyle(fontSize: 16,color: Color(0xff1B7997),fontFamily: 'Product Sans'),
         ));
       }
     }
@@ -84,106 +83,130 @@ class _BellyGPTPageState extends State<BellyGPTPage> {
     return widgets;
   }
 
-void _showInputBottomSheet(BuildContext context) {
+  void _showInputBottomSheet(BuildContext context) {
     showModalBottomSheet(
       context: context,
       backgroundColor: Colors.transparent,
       isScrollControlled: true,
       builder: (context) {
-        return Container(
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-          decoration: ShapeDecoration(
-            color: Colors.white,
-            shape: SmoothRectangleBorder(
-              borderRadius: SmoothBorderRadius.only(
-                topLeft: SmoothRadius(cornerRadius: 20, cornerSmoothing: 1),
-                topRight: SmoothRadius(cornerRadius: 20, cornerSmoothing: 1),
+        return Padding(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+            decoration: ShapeDecoration(
+              color: Colors.white,
+              shape: SmoothRectangleBorder(
+                borderRadius: SmoothBorderRadius.only(
+                  topLeft: SmoothRadius(cornerRadius: 40, cornerSmoothing: 1),
+                  topRight: SmoothRadius(cornerRadius: 40, cornerSmoothing: 1),
+                ),
               ),
             ),
-          ),
-          child: Wrap(
-            children: [
-              Column(
-                children: [
-                  SizedBox(height: 10),
-                  Container(
-                    width: 40,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(12),
+            child: Wrap(
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 5,
+                      decoration: BoxDecoration(
+                        color: Color(0xffFA6E00).withOpacity(0.50),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Write your requirement here..',
+                    SizedBox(height: 15),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Write your requirement here..',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Color(0xff1B7997),
+                          fontFamily: 'Product Sans',
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      controller: _promptController,
                       style: TextStyle(
-                        fontSize: 14,
-                        color: Color(0xff1B7997),
+                        color: Color(0xff0A4C61),
+                        height: 1.2,
+                        fontSize: 20,
                         fontFamily: 'Product Sans',
+                        fontWeight: FontWeight.bold,
+                      ),
+                      cursorColor: Color(0xff0A4C61),
+                      maxLines: null, // Allow the TextField to expand
+                      decoration: InputDecoration(
+                        hintMaxLines: 3,
+                        hintStyle: TextStyle(
+                          color: Color(0xff0A4C61).withOpacity(0.4),
+                          height: 1.2,
+                          fontSize: 20,
+                          fontFamily: 'Product Sans',
+                          fontWeight: FontWeight.bold,
+                        ),
+                        hintText:
+                            'Give me a diet plan to gain weight, I am allergic to milk. What should I eat?',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[200],
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    controller: _promptController,
-                    maxLines: 3,
-                    decoration: InputDecoration(
-                      hintStyle: TextStyle(color: Color(0xff0A4C61).withOpacity(0.4),height: 1.2,fontSize: 20,fontFamily: 'Product Sans',fontWeight: FontWeight.bold),
-                      hintText:
-                          'Give me a diet plan to gain weight, I am allergic to milk. What should I eat?',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide.none,
-                      ),
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  GestureDetector(
-                    onTap: _askBellyGPT,
-                    child: Center(
-                      child: Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        decoration: ShapeDecoration(
-                          shadows: [
-                            BoxShadow(
-                              offset: const Offset(5, 6),
-                              color: Color(0xffFA6E00).withOpacity(0.51),
-                              blurRadius: 30,
-                            ),
-                          ],
-                          color: Color(0xffFA6E00),
-                          shape: SmoothRectangleBorder(
-                            borderRadius: SmoothBorderRadius(
-                              cornerRadius: 15,
-                              cornerSmoothing: 1,
+                    SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: _isLoading ? null : _askBellyGPT, // Prevent multiple taps
+                      child: Center(
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                          decoration: ShapeDecoration(
+                            shadows: [
+                              BoxShadow(
+                                offset: const Offset(5, 6),
+                                color: Color(0xffFA6E00).withOpacity(0.51),
+                                blurRadius: 30,
+                              ),
+                            ],
+                            color: _isLoading ? Colors.grey : Color(0xffFA6E00), // Disable color if loading
+                            shape: SmoothRectangleBorder(
+                              borderRadius: SmoothBorderRadius(
+                                cornerRadius: 15,
+                                cornerSmoothing: 1,
+                              ),
                             ),
                           ),
-                        ),
-                        child: Text(
-                          'Ask bellyGPT',
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontFamily: 'Product Sans',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
+                          child: _isLoading
+                              ? CircularProgressIndicator(
+                                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                                )
+                              : Text(
+                                  'Ask bellyGPT',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontFamily: 'Product Sans',
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         );
       },
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -277,7 +300,7 @@ void _showInputBottomSheet(BuildContext context) {
                         fontSize: 16,
                         fontFamily: 'Product Sans',
                         fontWeight: FontWeight.bold,
-                        color: Colors.white),
+                                               color: Colors.white),
                   ),
                 ),
               ),
